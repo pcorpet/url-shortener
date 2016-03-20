09882ce3bf0cf930f3a16ab8e046eceab60d487f
Pascal Corpet <pascal+github@corpet.net>
2016-03-20 22:25:20 +0100

    Use docker on CircleCI and push builds on Docker Hub when tests succeed.

--- build.sh
+++ build.sh
@@ -8,8 +8,12 @@ cp -R public "${TMPDIR}/public"
 cp Dockerfile "${TMPDIR}"
 
 echo "Building binary..."
+if [ -z "${CIRCLECI}" ]; then
+  # `docker rm` doesn't work on CircleCI.
+  readonly RM_FLAG="--rm"
+fi
 mkdir -p release
-docker-compose run --rm -e CGO_ENABLED=0 builder /bin/bash -c "cp /etc/ssl/certs/ca-certificates.crt release && go build -ldflags \"-s\" -a -installsuffix cgo -o release/url-shortener"
+docker-compose run ${RM_FLAG} -e CGO_ENABLED=0 builder /bin/bash -c "cp /etc/ssl/certs/ca-certificates.crt release && go build -ldflags \"-s\" -a -installsuffix cgo -o release/url-shortener"
 cp release/url-shortener release/ca-certificates.crt "${TMPDIR}"
 
 echo "Packaging Docker image..."
